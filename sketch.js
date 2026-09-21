30d0b5224b1bdb18851f99a0a70983f02242888e
BL33ND4N0 <obiekechealle@gmail.com>
2026-09-21 19:30:54 +0100

    TODO: Bounce Mechanic & Z-index bounds scaling

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
 const Stage_C_V = { // I was originally into the idea of a context variable, but seeing now, Might as well have just created classes, though, Ousterhoust said it simply mitigates stuff, so honestly fairs.
     Stage_Width: window.innerWidth - 100,
     Stage_Height: window.innerHeight - 100,
+    getPos(){
+
+        const [x1,x2,y1,y2,z1,z2] = 
+        [
+            -this.Stage_Width,
+            this.Stage_Width,
+            -this.Stage_Height,
+            this.Stage_Width,
+            -this.Stage_Width * 0.83,
+            this.Stage_Width * 0.83,
+        ]
+
+        const coords = [[x1, x2], [y1, y2], [z1, z2]]
+
+        return coords
+    }
 }
+
 
 const Pong_C_V = {
     Pong_x : 0,
@@ -21,13 +38,13 @@ const Pong_C_V = {
             this.Pong_z + this.Pong_Depth / 2
         ]
 
-        const coords = [x1, x2, y1, y2, z1, z2]
+        const coords = [[x1, x2], [y1, y2], [z1, z2]]
         return coords
     }
 }
 
 const Ping_C_V= {
-    Ping_diameter: 45,
+    Ping_radius: 45,
     Ping_x: 0,
     Ping_y: window.innerHeight / 8,
     Ping_z: -500,
@@ -35,15 +52,29 @@ const Ping_C_V= {
     UpOrDown: "Up",
     LeftOrRight: "Left",
     Game_Started: false, // Game start belongs here because on a game reset, this is the only thing I plan to affect
+    getPos(){
+        const [x1,x2,y1,y2,z1,z2] = [
+            this.Ping_x - this.Ping_radius,
+            this.Ping_x + this.Ping_radius,
+            this.Ping_y - this.Ping_radius,
+            this.Ping_y + this.Ping_radius,
+            this.Ping_z - this.Ping_radius,
+            this.Ping_z + this.Ping_radius        
+        ] // returns a box for position
+
+        const coords = [[x1, x2], [y1, y2], [z1, z2]]
+
+        return coords
+    },
     move: (PingBall) => { 
         if (PingBall.Game_Started == true){
             switch (PingBall.LeftOrRight.toLowerCase()){
 
             case "right":
-                PingBall.Ping_x += PingBall.Ping_Speed
+                PingBall.Ping_y -= PingBall.Ping_Speed
                 break;
             case "left":
-                PingBall.Ping_x -= PingBall.Ping_Speed
+                PingBall.Ping_y += PingBall.Ping_Speed
                 break;
 
         }
@@ -102,11 +133,23 @@ document.addEventListener('keydown', (event) => {
     }
 });
 
+
+
 function collision(entity1, entity2){
-    
+    const [e1x, e1y, e1z] = entity1.getPos();
+    const [e2x, e2y, e2z] = entity2.getPos();
+
+
+    const overlapX = e1x[0] <= e2x[1] && e1x[1] >= e2x[0];
+    const overlapY = e1y[0] <= e2y[1] && e1y[1] >= e2y[0];
+    const overlapZ = e1z[0] <= e2z[1] && e1z[1] >= e2z[0];
+
+    if (overlapX && overlapY && overlapZ) {
+        console.log("They Collided.");
+        return true;
+    }
+
 }
-
-
 
 function setup() {
     createCanvas(Stage_C_V.Stage_Width, Stage_C_V.Stage_Height, WEBGL);
@@ -126,12 +169,12 @@ function draw_pong(x, y, z,width,height,depth){
 }
 
 
-function draw_ping(x,y,z,diameter){
+function draw_ping(x,y,z,radius){
 
     push();
     translate(x,y,z)
     fill("yellow")
-    sphere(diameter, 10, 4)
+    sphere(radius, 10, 4)
     pop();
 }
 
@@ -139,11 +182,14 @@ function draw_ping(x,y,z,diameter){
 function draw() {
 
     background(220);
+
     draw_pong(Pong_C_V.Pong_x, Pong_C_V.Pong_y, Pong_C_V.Pong_z,Pong_C_V.Pong_Width,Pong_C_V.Pong_Height,Pong_C_V.Pong_Depth)
-    draw_ping(Ping_C_V.Ping_x, Ping_C_V.Ping_y ,Ping_C_V.Ping_z, Ping_C_V.Ping_diameter)
+    draw_ping(Ping_C_V.Ping_x, Ping_C_V.Ping_y ,Ping_C_V.Ping_z, Ping_C_V.Ping_radius)
     Ping_C_V.move(Ping_C_V)
 
-    console.log(Pong_C_V.getPos())
+    if(collision(Ping_C_V, Pong_C_V)){
+        
+    }
 
 }
 
